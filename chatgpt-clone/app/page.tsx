b4e1836a683f8483cc59fd9da402b7406a98334f
DuzDao <duzdao.workspace@gmail.com>
2025-01-08 14:48:34 +0700

    feat: add login to home page

--- chatgpt-clone/app/page.tsx
+++ chatgpt-clone/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
       <div>This is homepage</div>
       <a href="\auth\signup">Signup</a>
+      <a href="\auth\login">Login</a>
     </>
   );
 }
