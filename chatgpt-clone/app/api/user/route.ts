8877c4d1dd86a0995b58504741b9d7f1f4bf8407
DuzDao <duzdao.workspace@gmail.com>
2025-01-09 16:08:38 +0700

    feat: get all conversations's title of a user

---
+++ chatgpt-clone/app/api/user/route.ts
@@ -0,0 +1,11 @@
+import { prisma } from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await prisma.conversation.findMany(
+    { where: {userId: "cm5nalsb40007hdhgeg15jpbc"}} //Get user id of user named dung for testing
+  )
+  return NextResponse.json({
+    message: res
+  })
+}
